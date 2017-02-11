6a29d8157d245e0b63f5e3eedeafdc2a307147c6
kevinhuang78 <kevin_huang_iim@yahoo.com>
2017-02-11 22:23:45 +0100

    Ajout de l'upload d'image pour le profil

--- resources/views/user/profil.blade.php
+++ resources/views/user/profil.blade.php
@@ -9,25 +9,27 @@
 
                 <div class="panel-body">
                     <img src="{{$user->img}}" style="width : 200px; height : auto;" alt="img-article">
-                    @if (Auth::user()->id)
-                      {!! Form::open(
-                          array(
-                              'route' => 'user.edit_img', 
-                              'class' => 'form', 
-                              'novalidate' => 'novalidate', 
-                              'files' => true)) !!}
-                        <div class="form-group">
-                            {!! Form::label('Image') !!}
-                            {!! Form::file('image', null) !!}
-                        </div>
-                        <div class="form-group">
-                            {!! Form::submit('Upload Image!') !!}
-                        </div>
-                      {!! Form::close() !!}
+                    @if (Auth::check())
+                      @if (Auth::user()->id)
+                        {!! Form::open(
+                            array(
+                                'route' => 'user.edit_img', 
+                                'class' => 'form', 
+                                'novalidate' => 'novalidate', 
+                                'files' => true)) !!}
+                          <div class="form-group">
+                              {!! Form::label('Image') !!}
+                              {!! Form::file('image', null) !!}
+                          </div>
+                          <div class="form-group">
+                              {!! Form::submit('Upload Image!') !!}
+                          </div>
+                        {!! Form::close() !!}
+                      @endif
+                      <p>You are logged as {{$user->name}} !</p>
+                      <p>Your email is  {{$user->email}}.</p>
+                      <p>Session created {{$user->created_at}} .</p>
                     @endif
-                    <p>You are logged as {{$user->name}} !</p>
-                    <p>Your email is  {{$user->email}}.</p>
-                    <p>Session created {{$user->created_at}} .</p>
                 </div>
             </div>
         </div>
@@ -47,19 +49,23 @@
                 <td> {{$article->title}} </td>
                 <td> {{$article->content}} </td>
                 <td>
+                @if (Auth::check())
                     @if (Auth::user()->isAdmin == 1 || Auth::user()->id == $article->user_id)
                   <form action='{{ route('articles.edit', ['id' => $article->id]) }}' method="get">
                       <input name="_token" type="hidden" value="{{ csrf_token() }}"/>
                           <button type="submit" class="btn btn-success"> Edit </button>
                   </form>
+                  @endif
               @endif
                   </td>
                   <td>
+                  @if (Auth::check())
                       @if (Auth::user()->isAdmin == 1 || Auth::user()->id == $article->user_id)
                 <form action='{{ route('articles.delete', ['id' => $article->id]) }}' method="post">
                   <input name="_token" type="hidden" value="{{ csrf_token() }}"/>
                       <button type="submit" class="btn btn-warning"> Delete </button>
                 </form>
+                @endif
             @endif
               </td>
               </tr>
