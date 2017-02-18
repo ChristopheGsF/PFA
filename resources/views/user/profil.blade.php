b6bef76a45de424b773ef71fb6ca29f6cf2c2c7b
kevinhuang78 <kevin_huang_iim@yahoo.com>
2017-02-18 13:21:37 +0100

    Ajout de Edit en fonction de si administrateur, user ou non

--- resources/views/user/profil.blade.php
+++ resources/views/user/profil.blade.php
@@ -2,6 +2,25 @@
 @include('messages.success')
 
 @section('content')
+<script>
+$(document).ready(function(){
+  $("a").on('click', function(event) 
+              {
+                if (this.hash !== "") 
+                {
+                  event.preventDefault();
+                  var hash = this.hash;
+                  $('html, body').animate(
+                  {
+                    scrollTop: $(hash).offset().top
+                  }, 800, function()
+                  {
+                    window.location.hash = hash;
+                  });
+                } 
+              });
+});
+</script>
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
@@ -9,27 +28,56 @@
                 <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
-                    <img src="{{$user->img}}" style="width : 200px; height : auto;" alt="img-article">
+                    <h1 class="text-center">{{$user->name}}</h1>
+                    @if ($user->img)
+                      <img src="/{{$user->img}}" style="width : 200px; height : auto; float:left;" alt="img-article">
+                    @else
+                      <div class="col-md-6">
+                        <p>Cet(te) utilisateur n'a pas d'image de profil pour l'instant</p>
+                      </div>
+                    @endif
                     @if (Auth::check())
-                      @if (Auth::user()->id)
-                        {!! Form::open(
-                            array(
-                                'route' => 'user.edit_img',
-                                'class' => 'form',
-                                'novalidate' => 'novalidate',
-                                'files' => true)) !!}
-                          <div class="form-group">
-                              {!! Form::label('Image') !!}
-                              {!! Form::file('image', null) !!}
+                      @if (Auth::user()->id == $user->id)
+                        <div class="col-md-6">
+                          <div class="col-md-12">
+                            <p>Tu es connecté en tant que : {{$user->name}} !</p>
+                            <p>Ton email est : {{$user->email}}.</p>
+                            <p>Compte crée le : {{$user->created_at}}.</p>
                           </div>
-                          <div class="form-group">
-                              {!! Form::submit('Upload Image!') !!}
+                          <div class="col-md-12">
+                            {!! Form::open(
+                                array(
+                                    'route' => 'user.edit_img',
+                                    'class' => 'form',
+                                    'novalidate' => 'novalidate',
+                                    'files' => true)) !!}
+                              <div class="form-group">
+                                  {!! Form::label("Pour changer d'image de profil") !!}
+                                  {!! Form::file('image', null) !!}
+                              </div>
+                              <div class="form-group">
+                                  {!! Form::submit('Télécharger une image !') !!}
+                              </div>
+                            {!! Form::close() !!}
                           </div>
-                        {!! Form::close() !!}
+                        </div>
+                      @elseif (Auth::user()->isAdmin)
+                        <div class="col-md-6">
+                          <div class="col-md-12">
+                            <p>En tant qu'administrateur tu peux modifier à loisir les articles de {{$user->name}} !</p>
+                            <p>En cliquant juste ici ou en regardant plus bas !</p>
+                            <a href="#articles"><img src="http://icon-icons.com/icons2/936/PNG/512/hand-finger-pointing-down_icon-icons.com_73538.png" alt="Doigt qui pointe vers le bas" style="width : 100px; height : auto;"></a>
+                          </div>
+                        </div>
+                      @else
+                        <div class="col-md-6">
+                          <div class="col-md-12">
+                            <p>Tu pourras voir ici tous les articles écrites par {{$user->name}} !</p>
+                            <p>Intéressant ? Non ?</p>
+                            <a href="#articles"><img src="http://icon-icons.com/icons2/936/PNG/512/hand-finger-pointing-down_icon-icons.com_73538.png" alt="Doigt qui pointe vers le bas" style="width : 100px; height : auto;"></a>
+                          </div>
+                        </div>
                       @endif
-                      <p>You are logged as {{$user->name}} !</p>
-                      <p>Your email is  {{$user->email}}.</p>
-                      <p>Session created {{$user->created_at}} .</p>
                     @endif
                 </div>
             </div>
@@ -37,7 +85,7 @@
     </div>
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
-          <table class="table table-hover">
+          <table class="table table-hover" id="articles">
             <thead>
               <tr>
                 <td> Titre </td>
