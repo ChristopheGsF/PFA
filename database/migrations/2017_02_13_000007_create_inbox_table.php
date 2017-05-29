d7a825cd229c2787feb566596e62bf81bfd2c645
Christophegsf <christophe.goncalvesdafonseca@gmail.com>
2017-05-29 16:05:17 +0000

    patch correctif

---
+++ database/migrations/2017_02_13_000007_create_inbox_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInboxTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      Schema::create('inboxes', function (Blueprint $table) {
+        $table->increments('id');
+        $table->text('content');
+        $table->integer('hash_id')->unsigned();
+        $table->foreign('hash_id')->references('id')->on('inboxe_groups');
+        $table->integer('user_id')->unsigned();
+        $table->foreign('user_id')->references('id')->on('users');
+        $table->timestamps();
+      });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+      Schema::dropIfExists('inboxes');
+    }
+}
