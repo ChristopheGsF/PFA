a0208e78a6fc0bd1832554f76e329481f37908be
lexahrro <alexandre.herrero@live.fr>
2017-05-29 12:48:32 +0200

    fix profil

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,7 +33,7 @@
                 <div class="navbar-header">
 
                     <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                    <button type="button" class="navbar-toggle collapsed center-block visible-xs" data-toggle="collapse" data-target="#app-navbar-collapse">
                         <span class="sr-only">Toggle Navigation</span>
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
--- resources/views/user/profil.blade.php
+++ resources/views/user/profil.blade.php
@@ -27,6 +27,20 @@ $(document).ready(function(){
             $("#profil_edition").slideToggle(300);
         });
     });
+
+    $(document).ready(function(){
+        $("#btn-profil-like").click(function(){
+            $("#profil_likes").slideToggle(300);
+        });
+    });
+
+    $(document).ready(function(){
+        $("#btn-profil-annonces").click(function(){
+            $("#profil_annonces").slideToggle(300);
+        });
+    });
+
+
 </script>
 
 <section class="section-profil">
@@ -60,7 +74,7 @@ $(document).ready(function(){
 
     </div>
     <div class="row row_button_edit">
-        <a href="#" id="btn-edit-profil" class="btn btn-default center-block link_add"><span class="glyphicon glyphicon-pencil"></span> Edition profil</a>
+        <a id="btn-edit-profil" class="btn btn-default center-block link_add"><span class="glyphicon glyphicon-pencil"></span> Edition profil</a>
     </div>
     <div id="profil_edition">
         {!! Form::open(
@@ -81,158 +95,115 @@ $(document).ready(function(){
 </section>
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<div class="container">
+<section class="section-liked">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading text-center">Mon profil</div>
+        <div class="col-md-12">
+            <h3 class="text-center occasion_text white">LIKES</h3>
+        </div>
+        <a class="plus_link" id="btn-profil-like"><button  class="btn-plus center-block"><span class="plus glyphicon glyphicon-plus"></span></button>
+        </a>
+    </div>
+    <div id="profil_likes">
+        <div class="container">
+            <div class="row">
+                @foreach ($likes as $like)
+                    @foreach ($posts as $post)
+                        @if( $like->article_id == $post->id)
+                <div class="col-md-4">
 
-                <div class="panel-body">
+                        <a href="{{ route('articles.show', ['id' => $post->id]) }}"><img src="{{$post->img}}" class="img-responsive" alt="img-article"></a>
+                        <h2 class="article-liked text-center">{{$post->title}}</h2>
+                        <p class="text-center" style="color: #fff;"> <span class="glyphicon glyphicon-time"></span> {{$post->release}}</p>
 
-                    @if ($user->img)
-                      <img src="{{$user->img}}" style="width : 200px; height : auto; float:left;" alt="img-article">
-                    @else
-                      <div class="col-md-6">
-                        <p>Cet(te) utilisateur n'a pas d'image de profil pour l'instant</p>
-                      </div>
-                    @endif
-                    @if (Auth::check())
-                      @if (Auth::user()->id == $user->id)
-                        <div class="col-md-6">
-                          <div class="col-md-12">
-
-                            <p>Tu es bien connecté en tant que : {{$user->name}}</p>
-                            <p>Ton email est : {{$user->email}}.</p>
-                            <p>Ton compte a été crée le : {{$user->created_at}}</p>
-                          </div>
-                          <div class="col-md-12">
-                            {!! Form::open(
-                                array(
-                                    'route' => 'user.edit_img',
-                                    'class' => 'form',
-                                    'novalidate' => 'novalidate',
-                                    'files' => true)) !!}
-                              <div class="form-group">
-                                  {!! Form::label("Pour changer d'image de profil") !!}
-                                  {!! Form::file('image', null) !!}
-                              </div>
-                              <div class="form-group">
-                                  {!! Form::submit('Télécharger une image !') !!}
-                              </div>
-                            {!! Form::close() !!}
-                          </div>
-                        </div>
-                      @elseif (Auth::user()->isAdmin)
-                        <div class="col-md-6">
-                          <div class="col-md-12">
-                            <p>En tant qu'administrateur tu peux modifier à loisir les articles de {{$user->name}} !</p>
-                            <p>En cliquant juste ici ou en regardant plus bas !</p>
-                            <a href="#articles"><img src="http://icon-icons.com/icons2/936/PNG/512/hand-finger-pointing-down_icon-icons.com_73538.png" alt="Doigt qui pointe vers le bas" style="width : 100px; height : auto;"></a>
-                          </div>
-                        </div>
-                      @else
-                        <div class="col-md-6">
-                          <div class="col-md-12">
-                            <p>Tu pourras voir ici tous les articles écrites par {{$user->name}} !</p>
-                            <p>Intéressant ? Non ?</p>
-                            <a href="#articles"><img src="http://icon-icons.com/icons2/936/PNG/512/hand-finger-pointing-down_icon-icons.com_73538.png" alt="Doigt qui pointe vers le bas" style="width : 100px; height : auto;"></a>
-                          </div>
-                        </div>
-                      @endif
-                    @endif
-                    @if (!Auth::check())
-                      <div class="col-md-6">
-                          <div class="col-md-12">
-                            <p>Tu pourras voir ici tous les articles écrites par {{$user->name}} !</p>
-                            <p>Intéressant ? Non ?</p>
-                            <a href="#articles"><img src="http://icon-icons.com/icons2/936/PNG/512/hand-finger-pointing-down_icon-icons.com_73538.png" alt="Doigt qui pointe vers le bas" style="width : 100px; height : auto;"></a>
-                          </div>
-                        </div>
-                    @endif
                 </div>
+                        @endif
+                    @endforeach
+                @endforeach
             </div>
         </div>
     </div>
+</section>
+
+<section class="section-annonces">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-          <table class="table table-hover" id="articles">
-            <thead>
-              <tr>
-                <td> Titre </td>
-                <td> Content </td>
-              </tr>
-            </thead>
-            <tbody>
-              @foreach ($articles as $article)
-              <tr>
-                <td><a href="{{ route('articles.index') }}/{{$article->id}}/show">{{$article->title}}</a></td>
-                <td> {{$article->content}} </td>
-                <td>
+        <div class="col-md-12">
+            <h3 class="text-center occasion_text white">Mes Annonces</h3>
+        </div>
+        <a class="plus_link" id="btn-profil-annonces"><button  class="btn-plus center-block"><span class="plus glyphicon glyphicon-plus"></span></button>
+        </a>
+    </div>
+    <div id="profil_annonces">
+        <div class="container">
+            <div class="row hidden-xs">
+                <div class="col-md-3">
+                    <h3>Image</h3>
+                </div>
+                <div class="col-md-2">
+                    <h3>Détails</h3>
+                </div>
+                <div class="col-md-6">
+                    <h3>Description</h3>
+                </div>
+                <div class="col-md-1">
+                    <h3>Actions</h3>
+                </div>
+            </div>
+            <hr class="hr-dark">
+            @foreach ($articles as $article)
+        <div class="row">
+            <div class="col-md-3">
+                <img src="{{$article->img}}" class="img-responsive center-block" alt="img-article">
+            </div>
+            <div class="col-md-2">
+                <a href="{{ route('articles.index') }}/{{$article->id}}/show"><h3>{{$article->brand}}</h3></a>
+                <h5><strong>Modèle :</strong> {{$article->model}}</h5>
+                <h5><strong>Prix :</strong> {{$article->price}}</h5>
+                <h5><strong>Taille :</strong> {{$article->size}}</h5>
+            </div>
+            <div class="col-md-6">
+                {{$article->content}}
+            </div>
+            <div class="col-md-1">
                 @if (Auth::check())
                     @if (Auth::user()->isAdmin == 1 || Auth::user()->id == $article->user_id)
-                  <form action='{{ route('articles.edit', ['id' => $article->id]) }}' method="get">
-                      <input name="_token" type="hidden" value="{{ csrf_token() }}"/>
-                          <button type="submit" class="btn btn-success"> Edit </button>
-                  </form>
-                  @endif
-              @endif
-                  </td>
-                  <td>
-                  @if (Auth::check())
-                      @if (Auth::user()->isAdmin == 1 || Auth::user()->id == $article->user_id)
-                <form action='{{ route('articles.delete', ['id' => $article->id]) }}' method="post">
-                  <input name="_token" type="hidden" value="{{ csrf_token() }}"/>
-                      <button type="submit" class="btn btn-warning"> Delete </button>
-                </form>
-                @endif
-            @endif
-              </td>
-              </tr>
-            @endforeach
-            </tbody>
-          </table>
-          {{ $articles->links() }}
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-
-                @foreach ($likes as $like)
-                @foreach ($posts as $post)
-                    @if( $like->article_id == $post->id)
-               <a href="{{ route('articles.show', ['id' => $post->id]) }}"><img src="{{$post->img}}" class="img-responsive" alt="img-article"></a>
-               {{$post->title}}
+                        <form action='{{ route('articles.edit', ['id' => $article->id]) }}' method="get">
+                            <input name="_token" type="hidden" value="{{ csrf_token() }}"/>
+                            <button type="submit" class="btn btn_edit"><span class="plus glyphicon glyphicon-pencil"></span></button>
+                        </form>
                     @endif
-                @endforeach
-                @endforeach
+                @endif
 
-            {{ $articles->links() }}
+                    @if (Auth::check())
+                        @if (Auth::user()->isAdmin == 1 || Auth::user()->id == $article->user_id)
+                            <form action='{{ route('articles.delete', ['id' => $article->id]) }}' method="post">
+                                <input name="_token" type="hidden" value="{{ csrf_token() }}"/>
+                                <button type="submit" class="btn btn_delete"><span class="plus glyphicon glyphicon-trash"></span></button>
+                            </form>
+                        @endif
+                    @endif
+
+            </div>
+
+
+        </div>
+                <hr class="hr-dark">
+            @endforeach
         </div>
     </div>
-</div>
+</section>
 @endsection
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
